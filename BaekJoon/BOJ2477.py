519302e1ecdd7d16654f20bd141a0b3630eea8fe
woosteelz <woosteelz@naver.com>
2021-07-27 23:09:19 +0900

    ADD [BOJ/~.py]

--- BaekJoon/BOJ2477.py
+++ BaekJoon/BOJ2477.py
@@ -1,1 +1,15 @@
 # 2477번 참외밭
+melon = int(input())
+length = []
+for _ in range(6):
+    a, b = map(int, input().split())
+    length.append(b)
+
+idx = length.index(max(length))
+
+if length[idx-1] < length[(idx+1) % 6]:
+    idx2 = (idx+1) % 6
+else:
+    idx2 = idx-1
+
+print((length[idx] * length[idx2] - length[idx-3] * length[idx2-3]) * melon)
---
+++ BaekJoon/BOJ2527(,,ing).py
@@ -0,0 +1,5 @@
+# 2527번 직사각형
+for _ in range(4):
+    temp = list(map(int, input().split()))
+    x_dir = temp[::2]
+    y_dir = temp[1::2]
