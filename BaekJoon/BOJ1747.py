0407a765dc733a9e0561d165466a0567d0dc417d
woosteelz <woosteelz@naver.com>
2021-09-24 00:43:50 +0900

    0924 MODIFY [BOJ/1747]

--- BaekJoon/BOJ1747.py
+++ BaekJoon/BOJ1747.py
@@ -1,9 +1,10 @@
 # 1747번 소수 & 펠린드롬
+import math
 
 is_prime = [True for _ in range(1005000)]
 is_prime[0], is_prime[1] = False, False
 
-for i in range(2, 1005000):
+for i in range(2, int(math.sqrt(1005000))):
     if is_prime[i]:
         for j in range(2*i, 1005000, i):
             is_prime[j] = False
