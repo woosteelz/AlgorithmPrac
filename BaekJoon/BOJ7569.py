0ca9dc58752a9b2de38ec940d28f2d52bc059dd9
woosteelz <woosteelz@naver.com>
2021-09-23 22:57:39 +0900

    0923 MODIFY [BOJ/7569]

--- BaekJoon/BOJ7569.py
+++ BaekJoon/BOJ7569.py
@@ -3,7 +3,8 @@ import sys
 from collections import deque
 
 m, n, h = map(int, sys.stdin.readline().split())
-graph = [[list(map(int, sys.stdin.readline().split())) for _ in range(n)] for _ in range(h)]
+graph = [[list(map(int, sys.stdin.readline().split()))
+          for _ in range(n)] for _ in range(h)]
 visited = [[[False]*m for _ in range(n)] for _ in range(h)]
 
 dir_x = [1, -1, 0, 0, 0, 0]
@@ -11,34 +12,33 @@ dir_y = [0, 0, 1, -1, 0, 0]
 dir_z = [0, 0, 0, 0, 1, -1]
 
 queue = deque()
-def BFS():
-	while queue:
-		x, y, z = queue.popleft()
-		for i in range(6):
-			nx = x + dir_x[i]
-			ny = y + dir_y[i]
-			nz = z + dir_z[i]
-			if 0<=nx<h and 0<=ny<n and 0<=nz<m:
-				if not visited[nx][ny][nz] and graph[nx][ny][nz] == 0:
-					queue.append([nx, ny, nz])
-					visited[nx][ny][nz] = True
-					graph[nx][ny][nz] = graph[x][y][z] + 1
+
 
 for i in range(h):
-	for j in range(n):
-		for k in range(m):
-			if graph[i][j][k] == 1 and not visited[i][j][k]:
-				queue.append([i, j, k])
-				visited[i][j][k] = True
+    for j in range(n):
+        for k in range(m):
+            if graph[i][j][k] == 1 and not visited[i][j][k]:
+                queue.append([i, j, k])
+                visited[i][j][k] = True
 
-BFS()
+while queue:
+    x, y, z = queue.popleft()
+    for i in range(6):
+        nx = x + dir_x[i]
+        ny = y + dir_y[i]
+        nz = z + dir_z[i]
+        if 0 <= nx < h and 0 <= ny < n and 0 <= nz < m:
+            if not visited[nx][ny][nz] and graph[nx][ny][nz] == 0:
+                queue.append([nx, ny, nz])
+                visited[nx][ny][nz] = True
+                graph[nx][ny][nz] = graph[x][y][z] + 1
 
 for i in range(h):
-	for j in range(n):
-		for k in range(m):
-			if graph[i][j][k] == 0:
-				print(-1)
-				sys.exit()
+    for j in range(n):
+        for k in range(m):
+            if graph[i][j][k] == 0:
+                print(-1)
+                sys.exit()
 
 
 ans = 0
