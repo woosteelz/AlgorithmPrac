a34c241cc7660b5d7d9a156404fd56da91974111
woosteelz <woosteelz@naver.com>
2021-11-02 18:54:59 +0900

    1102 ADD [BOJ/5052, 14719]

--- BaekJoon/BOJ5052.py
+++ BaekJoon/BOJ5052.py
@@ -4,14 +4,67 @@ input = sys.stdin.readline
 
 
 class Node:
-    def __init__(self, data):
+    def __init__(self, key, data=None):
         self.data = data
-        self.next = [None for _ in range(10)]
+        self.key = key
+        self.next = dict()
+
+
+class Trie:
+    def __init__(self):
+        self.root = Node(None)
+
+    def insert(self, string):
+        curr = self.root
+        for char in string:
+            if not char in curr.next:
+                curr.next[char] = Node(char)
+            curr = curr.next[char]
+        curr.data = string
+
+    def search(self, string):
+        curr = self.root
+        for char in string:
+            if char in curr.next:
+                curr = curr.next[char]
+            else:
+                return False
+        if curr.data:
+            return True
+
+    def startwith(self, string):
+        curr = self.root
+
+        for p in string:
+            if p in curr.next:
+                curr = curr.next[p]
+            else:
+                return False
+
+        return True
+
+
+def solution(strings):
+    trie = Trie()
+    for string in strings:
+        if trie.startwith(string):
+            return False
+        else:
+            trie.insert(string)
+    return True
 
 
 for _ in range(int(input())):
     N = int(input())
-    nums = [input().strip() for _ in range(N)]
-    nums.sort(key=lambda x: (len(x), x))
+    strings = [input().strip() for _ in range(N)]
+    strings.sort(key=lambda x: (-len(x)))
+    # print(strings)
 
-    print(nums)
+    print('YES') if solution(strings) else print('NO')
+
+    for i in range(1, N):
+        if strings[i-1] == strings[:len(strings[i-1])]:
+            print('NO')
+            break
+    else:
+        print('YES')
