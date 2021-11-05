8e78348c06331231f397eefeaf60db32977970ed
woosteelz <woosteelz@naver.com>
2021-11-05 18:00:04 +0900

    1105 ADD PG

---
+++ Programmers/PG_targetNum.py
@@ -0,0 +1,19 @@
+# 프로그래머스 타겟넘버(level 2)
+answer = 0
+
+
+def solution(numbers, target):
+    dfs(numbers, 0, 0, target)
+    return answer
+
+
+def dfs(arr, _sum, cnt, target):
+    global answer
+    if cnt == len(arr) and _sum == target:
+        answer += 1
+        return
+    if cnt == len(arr):
+        return
+
+    dfs(arr, _sum + arr[cnt], cnt+1, target)
+    dfs(arr, _sum - arr[cnt], cnt+1, target)
