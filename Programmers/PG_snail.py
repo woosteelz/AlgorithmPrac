86aef36bbd1cb54cf0e3b83dbdb5497271ef218d
woosteelz <woosteelz@naver.com>
2022-02-23 23:12:50 +0900

    0223 ADD [BOJ / solution]

--- Programmers/PG_snail.py
+++ Programmers/PG_snail.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = []
-    graph = [[0 for _ in range(n)] for _ in range(n)]
+    map = [[0 for _ in range(n)] for _ in range(n)]
 
     curr_x = 0
     curr_y = -1
@@ -16,12 +16,12 @@ def solution(n):
             elif i % 3 == 2:
                 curr_x -= 1
                 curr_y -= 1
-            graph[curr_y][curr_x] = num
+            map[curr_y][curr_x] = num
             num += 1
 
     for i in range(n):
         for j in range(n):
-            if not graph[i][j]:
-                answer.append(graph[i][j])
+            if map[i][j] != 0:
+                answer.append(map[i][j])
 
     return answer
